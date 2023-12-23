6fc314a892748bbeec1400476e03683ae6403277
Ume <79046153+EarthGMS@users.noreply.github.com>
2023-12-23 19:27:46 +0700

    add the code for checking mode selection

--- 3Bit_Calculator/3Bit_Calculator.ino
+++ 3Bit_Calculator/3Bit_Calculator.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+//Pins used in the project
+int AddSwitch = 2;
+int MultiplySwitch = 3;
 
+//Modes for doing math operations
+bool AddMode;
+bool MultiplyMode
+
+void setup() {
+  pinMode(AddSwitch, INPUT);
+  pinMode(MultiplySwitch, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  //Check the mode selection
+  if (digitalRead(AddSwitch) == 1){
+    AddMode = true;
+  }
+  else{
+    AddMode = false;
+  }
+  if (digitalRead(MultiplySwitch) == 1){
+    MultiplyMode = true;
+  }
+  else{
+    MultiplyMode = false;
+  }
+  if (MultiplyMode == true && AddMode == true){
+    //Tells LCD light to send an error message
+  }
 
 }
