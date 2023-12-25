a89d17b6399e17255dd398250cfa9aafffa2b3e1
Ume <79046153+EarthGMS@users.noreply.github.com>
2023-12-25 20:08:24 +0700

    add code for divide operation

--- 3Bit_Calculator/3Bit_Calculator.ino
+++ 3Bit_Calculator/3Bit_Calculator.ino
@@ -4,11 +4,18 @@ int MultiplySwitch = 3;
 
 //Modes for doing math operations
 bool AddMode;
-bool MultiplyMode
+bool MultiplyMode;
+int Base10Count;
+int Base10Divide;
+int[] BinaryResult;
 
 void setup() {
   pinMode(AddSwitch, INPUT);
   pinMode(MultiplySwitch, INPUT);
+  pinMode(0, INPUT);
+  pinMode(1, INPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
 }
 
 void loop() {
@@ -27,7 +34,20 @@ void loop() {
     MultiplyMode = false;
   }
   if (MultiplyMode == true && AddMode == true){
-    //Tells LCD light to send an error message
+    //Tells LCD light to send an error message and break the entire script
   }
 
+  //Check the pins
+  for (int i = 0, i < 4, i++){
+    if (digitalRead(i) == 1){
+     Base10Count += 2^i; 
+     Base10Divide = Base10Count;
+    }
+  }
+  for (int i = 0){
+    if (Base10Divide >= 0){
+      BinaryResult[i] = Base10Divide % 2;
+      Base10Divide %= 2;
+    }
+  }
 }
