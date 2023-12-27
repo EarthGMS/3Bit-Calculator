3163273dd04c5cd12a04444eaf70657783f7ade3
Ume <79046153+EarthGMS@users.noreply.github.com>
2023-12-27 20:39:30 +0700

    Fix binary reading

--- 3Bit_Calculator/3Bit_Calculator.ino
+++ 3Bit_Calculator/3Bit_Calculator.ino
@@ -1,3 +1,11 @@
+/*
+THE 3-BIT CALCULATOR PROJECT
+MEMBER : PONGSAPAT B.
+DATE STARTED : 11 DEC 2023 
+DATE FINISHED : TBD
+PURPOSE : FOR EDUCATIONAL PURPOSE ON DEMONSTRATION ON ADDING AND SUBTRACTING NUMBERS IN BINARY
+*/
+
 //Pins used in the project
 int AddSwitch = 2;
 int MultiplySwitch = 3;
@@ -7,18 +15,24 @@ bool AddMode;
 bool MultiplyMode;
 int Base10Count;
 int Base10Divide;
-int[] BinaryResult;
+int BinaryResult[4];
 
 void setup() {
   pinMode(AddSwitch, INPUT);
   pinMode(MultiplySwitch, INPUT);
-  pinMode(0, INPUT);
-  pinMode(1, INPUT);
   pinMode(2, INPUT);
   pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  Base10Count = 0;
+  Base10Divide = 0;
+  //Start
 
   //Check the mode selection
   if (digitalRead(AddSwitch) == 1){
@@ -38,16 +52,25 @@ void loop() {
   }
 
   //Check the pins
-  for (int i = 0, i < 4, i++){
+  for (int i = 2; i < 8; i++){
     if (digitalRead(i) == 1){
-     Base10Count += 2^i; 
+     Base10Count += pow(2.0, i - 2.0); 
      Base10Divide = Base10Count;
     }
+    else{
+      continue;
+    }
   }
-  for (int i = 0){
+  for (int i = 0; i < 4; i++){
     if (Base10Divide >= 0){
       BinaryResult[i] = Base10Divide % 2;
       Base10Divide %= 2;
     }
+    else{
+      break;
+    }
   }
+  //Tells LCD about the result
+  Serial.println(Base10Count);
+  //End
 }
